ffaedb20d81971db86fde000d8284f756794a7b6
artneto <arthur_neto@icloud.com>
2022-01-11 19:44:49 +0000

    separate functions in a new file

--- hang.py
+++ hang.py
@@ -1,11 +1,7 @@
+from random import choice
 # This function holds the different stages of user guesses
 # and displays the graphic for a hangman.
 def create_chance_for_man():
-    hang = """
-    ____
-        |
-        |
-        -"""
     empty = """
     """
     head = """
@@ -33,4 +29,30 @@ def create_chance_for_man():
     /|\\
     / \\
     """
-    return [hang, empty, head, body, left_arm, right_arm, left_leg, right_leg]
+    return [empty, head, body, left_arm, right_arm, left_leg, right_leg]
+
+def print_initial_message():
+    # Displays the welcome prompt and introduces user to the quiz and rules,
+    # and takes an input from customer to start the quiz.
+    print('*********************************')
+    print("Let's Play Hangman!")
+    print('*********************************')
+def get_username():
+    name = input("Enter your name -> ")
+    print('Welcome', name, '!')
+def print_rules():
+    print(" ----------------------------------------- ")
+    print("The objective of the Hangman is to guess the secret word.")
+    print("You will need to try you letter at a time,")
+    print("but If the word does not contain the suggested letter")
+    print("the computer will draws one element of a hangman’s gallows")
+    print("Just a reminder, you only have 6 attempts, good luck!")
+
+# Gets a random word from my list_of_words and return the word
+# with the word in uppercase.
+def create_newords():
+    with open('newords.txt') as file:
+        lines = file.read()
+        list_of_words = lines.split('\n')
+
+    return choice(list_of_words).upper()
--- run.py
+++ run.py
@@ -1,83 +1,25 @@
-from random import choice
+from hang import create_chance_for_man
+from hang import print_initial_message
+from hang import get_username
+from hang import print_rules
+from hang import create_newords
 
-# Gets a random word from my list_of_words and return the word
-# with the word in uppercase.
-
-with open('newords.txt') as file:
-    lines = file.read()
-    list_of_words = lines.split('\n')
-
-word = choice(list_of_words).upper()
-# This function holds the different stages of user guesses
-# and displays the graphic for a hangman.
-hang = """
-____
-    |
-    |
-    -"""
-
-
-empty = """
-"""
-
-head = """
-    0
-"""
-
-body = """
-    0
-    |
-"""
-
-left_arm = """
-    0
-   /|
-"""
-right_arm = """
-    0
-   /|\\
-"""
-left_leg = """
-    0
-   /|\\
-   /
-"""
-right_leg = """
-    0
-   /|\\
-   / \\
-"""
-
-
-chance_for_man = [empty,
-                  head,
-                  body,
-                  left_arm,
-                  right_arm,
-                  left_leg,
-                  right_leg,
-                  ]
+chance_for_man = create_chance_for_man()
 
 win = 0
 wrong = 0
 letters_rg = ''
 letters_wrg = ''
-
-# Displays the welcome prompt and introduces user to the quiz and rules,
-# and takes an input from customer to start the quiz.
-print('*********************************')
-print("Let's Play Hangman!")
-print('*********************************')
-name = input("Enter your name -> ")
-print('Welcome', name, '!')
-print(" ----------------------------------------- ")
-print("The objective of the Hangman is to guess the secret word.")
-print("You will need to try you letter at a time,")
-print("but If the word does not contain the suggested letter")
-print("the computer will draws one element of a hangman’s gallows")
-print("Just a reminder, you only have 6 attempts, good luck!")
-
+print_initial_message()
+get_username()
+print_rules()
+hang = """
+    ____
+        |
+        |
+        -"""
 # Checking if user input matches the letter in the "secret word".
+word = create_newords()
 while win != len(word) and wrong != 6:
     message = ''
     for letter in word:
